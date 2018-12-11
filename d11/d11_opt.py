7b321cc4e6ae202056b666768f623dda62684dfb
Sebastian Pauka <spauka@gmail.com>
2018-12-11 17:21:44 +1100

    Make indices less confusing

--- d11/d11_opt.py
+++ d11/d11_opt.py
@@ -10,31 +10,25 @@ def calc_fuel(x, y, serial=3031):
     power = int(f"{power:05d}"[-3]) - 5
     return power
 
-grid = []
+grid = [[0]*301]
 for Y in range(1, 301):
-    line = []
+    line = [0]
     for X in range(1, 301):
-        s = calc_fuel(X, Y, 3031)
-        if X > 1:
-            s += line[-1]
+        s = calc_fuel(X, Y, 3031) + line[-1]
         line.append(s)
         
-    if Y > 1:
-        for X in range(1, 301):
-            line[X-1] += grid[-1][X-1]
+    for X in range(1, 301):
+        line[X] += grid[-1][X]
     grid.append(line)
 
 mval = 0
 mind = (-1, -1, -1)
 for size in range(1, 301):
     for X, Y in product(range(size, 301), range(size, 301)):
-        s = grid[Y-1][X-1]
-        if Y-size > 0:
-            s -= grid[Y-size-1][X-1]
-        if X-size > 0:
-            s -= grid[Y-1][X-size-1]
-        if X-size > 0 and Y-size > 0:
-            s += grid[Y-size-1][X-size-1]
+        s = grid[Y][X]
+        s -= grid[Y-size][X]
+        s -= grid[Y][X-size]
+        s += grid[Y-size][X-size]
         
         if s > mval:
             mval = s
